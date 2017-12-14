6490533e51c1fb4afea9e03aa9562e4762f52207
Marei Kikukawa <contact@calmery.me>
2017-12-14 19:18:07 +0900

    Add base of app

---
+++ index.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require( 'electron' )
+
+const createWindow = options => {
+  return new Promise( ( resolve, _ ) => {
+    app.on( 'ready', () => {
+      const window = new BrowserWindow( options )
+      resolve( window )
+    } )
+  } )
+}
+
+createWindow()
+  .then( window => {
+    window.loadURL( `file://${__dirname}/index.html` )
+  } )
